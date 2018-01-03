5e8e19dbc932c90e4aa96704aeacd6980f059949
madhukakumanu <madhukakumanu@yahoo.co.in>
2018-01-03 06:14:33 +0530

    Add files via upload

---
+++ server.R
@@ -0,0 +1,29 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    xlab <- ifelse(input$Show_xlab, " Number of Bins ", "")
+    main <- ifelse(input$main, " Histogram Of Bins ", "")
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, main = main, col = 'blue', border = 'white', xlab=xlab)
+    
+  })
+  
+})
